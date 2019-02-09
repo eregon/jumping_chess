94625292c5af475a5bbb0dc2e6289d84b6ac9ad2
Benoit Daloze <eregontp@gmail.com>
2019-02-09 22:16:37 +0100

    Add support for bright colors

--- lib/jumping_chess/color.rb
+++ lib/jumping_chess/color.rb
@@ -1,12 +1,14 @@
 COLORS = %i[
   black red green yellow blue purple cyan white
-].map.with_index { |c,i| [c, "\e[#{30+i}m"] }.to_h
-
+]
+NORMAL = COLORS.map.with_index { |c,i| [c, "\e[#{30+i}m"] }
+BRIGHT = COLORS.map.with_index { |c,i| [:"bright_#{c}", "\e[#{30+i};1m"] }
+ALL_COLORS = (NORMAL + BRIGHT).to_h
 RESET_COLOR = "\e[0m"
 
 def colorize(text, color)
   if color
-    "#{COLORS[color]}#{text}#{RESET_COLOR}"
+    "#{ALL_COLORS[color]}#{text}#{RESET_COLOR}"
   else
     "#{text}"
   end
