73b423dba0edc7750321d19f8de1753aaa8f5b69
Benoit Daloze <eregontp@gmail.com>
2019-02-10 14:50:25 +0100

    Add a couple workarounds to run on top of WebAssembly

--- mruby_build_config.rb
+++ mruby_build_config.rb
@@ -1,7 +1,23 @@
-MRuby::Build.new do |conf|
-  toolchain :gcc
-
+def add_gems(conf)
   conf.gembox 'default'
   conf.gem __dir__
   conf.gem github: 'iij/mruby-mtest'
+end
+
+MRuby::Build.new do |conf|
+  toolchain :gcc
+
+  add_gems(conf)
+end
+
+if system("which emcc", err: File::NULL, out: File::NULL)
+  MRuby::CrossBuild.new('emscripten') do |conf|
+    toolchain :clang
+    conf.cc.command = 'emcc'
+    conf.cc.flags = %w[-Os]
+    conf.linker.command = 'emcc'
+    conf.archiver.command = 'emar'
+
+    add_gems(conf)
+  end
 end
